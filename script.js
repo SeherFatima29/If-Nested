e69b414ecf4c51c9890937cd1a7c774f406797a3
seherfatima <156211654+SeherFatima29@users.noreply.github.com>
2024-07-05 10:53:59 +0500

    Add files via upload
    🤩

---
+++ script.js
@@ -0,0 +1,9 @@
+var gmail=prompt("Enter Your Gmail ?")
+var ans="Admin@gmail.com"
+ if(gmail === ans){
+   var Password=prompt("Enter Password")
+   var ans1="Admin"
+   if(Password === ans1){
+    alert("you're admin")
+   }
+ }
